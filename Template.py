f6864bf8ec34331990e12160b66771b12e12ab04
whjr2021 <91241332+whjr2021@users.noreply.github.com>
2021-09-27 19:27:10 +0530

    Update Template.py

--- Template.py
+++ Template.py
@@ -1,12 +1,24 @@
 import pygame
 pygame.init()
-#Create the function here
 
+# Define a function 'create_building'
+def                (height,x,y,r,g,b,windows):
   
-
-
-
-
+  # Provide values of x and y coordinates into 'Rect()' function 
+  building = pygame.Rect(     ,     ,60,height)
+  # Draw the 'building' object on screen
+  pygame.draw.rect(screen,(r,g,b),              )
+  
+  # Provide width and height of the 'door' object
+  door=pygame.Rect(x+15,340,       ,      )
+  # Draw the 'door' object on screen
+  pygame.draw.rect(           ,(0,0,0),door)
+  
+  # Set the upper limit of the range function to the given number of windows
+  for i in range(1,            ):
+    window=pygame.Rect(x+20,50*i+60,20,20)
+    pygame.draw.rect(screen,(255,255,0),window)
+  
 screen = pygame.display.set_mode((300,400))
 pygame.display.set_caption("Project C13")
 
@@ -18,7 +30,5 @@ while carryOn:
   screen.fill((36,90,190))
   create_building(400,100,100,255,0,0,4)
   
-    
   pygame.display.flip()
-    
 pygame.quit()
